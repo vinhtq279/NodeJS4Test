94684d0924bd54d0f1fb10dab225838ef60f7e2e
vinhtq279 <vinhtq279@gmail.com>
2022-05-11 08:44:34 +0700

    .

--- test/index.js
+++ test/index.js
@@ -2,42 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    //clearInterval(this.timerID);
-    //console.log(this.state.count);
-    //this.setState({count: this.state.count + 1});
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
+const Form = () => {
+  return(
+    <>
+    <form action="http://127.0.0.1:5000/submit-student-data" method="post">
+      First Name: <input type="text" name="firstName" />
+      Last Name: <input type="text" name="lastName" />
+      <input type="submit" />
+    </form>
+    </>
+  )
 }
 
 ReactDOM.render(
-  <Clock />,
+  <Form />,
   document.getElementById('root')
 );
